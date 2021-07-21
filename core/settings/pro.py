3d3b1ba42eccf039717e6c4bbab0581502c95f7a
fernandrobroy@gmail.com <fernandrobroygmail.com>
2021-07-21 12:52:04 -0500

    Se modificaron los dominios permitidos

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -6,10 +6,10 @@ ADMINS = (
     ('admin', 'admin@mail.com'),
 )
 
-ALLOWED_HOSTS = ['sophiaeternum.com', 'www.sophiaeternum.com']
+ALLOWED_HOSTS = ['bug-tracker.sophiaeternum.com', 'www.bug-tracker.sophiaeternum.com']
 
 SECURE_CONTENT_TYPE_NOSNIFF  = True 
 SECURE_BROWSER_XSS_FILTER = True 
-SECURE_SSL_REDIRECT  = True 
+SECURE_SSL_REDIRECT  = False 
 X_FRAME_OPTIONS = 'DENY'
 CSRF_COOKIE_SECURE = True
