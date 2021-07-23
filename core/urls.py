dacd43406aaf56a8163b251195fd073b9f031eb4
fernandrobroy@gmail.com <fernandrobroygmail.com>
2021-07-23 08:08:34 -0500

    2 Nueva config para despliegue

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 from django.urls import path, include  # add this
 from django.conf import settings 
-from django.config.urls.static import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
